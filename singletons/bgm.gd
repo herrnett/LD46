ef897b37a84d81e7f38630f735ee2b870a008f02
herrnett <tim@herrnett.de>
2020-09-13 01:36:56 +0200

    v 1.1

--- singletons/bgm.gd
+++ singletons/bgm.gd
@@ -2,9 +2,6 @@ extends AudioStreamPlayer
 
 onready var tween = get_node("Tween")
 
-export var transition_duration = 1.00
-export var transition_type = 1 # TRANS_SINE
-
 func fade_out():
 	# tween music volume down to 0
 	tween.interpolate_property(self, "volume_db", 0, -80, 5, Tween.TRANS_QUINT, Tween.EASE_OUT)
@@ -14,5 +11,5 @@ func fade_out():
 
 func fade_in():
 	self.playing = true
-	tween.interpolate_property(self, "volume_db", -80, 0, 6, Tween.TRANS_QUINT, Tween.EASE_OUT)
+	tween.interpolate_property(self, "volume_db", -80, 0, 2.5, Tween.TRANS_SINE, Tween.EASE_OUT)
 	tween.start()
