e23cff885f2f34d78db5d4cb62fe5bed585daa25
herrnett <tim@herrnett.de>
2020-09-13 03:16:39 +0200

    v 1.1

--- singletons/Scene_changer.gd
+++ singletons/Scene_changer.gd
@@ -4,9 +4,9 @@ onready var animation_player = $AnimationPlayer
 onready var colorrect = $ColorRect
 
 
-func change_scene(path, delay = 0.5, fadein = true):
+func change_scene(path, delay = 0.5, fadeoutfirst = true):
 	yield(get_tree().create_timer(delay), "timeout")
-	if fadein:
+	if fadeoutfirst:
 		animation_player.play("fade")
 		yield(animation_player, "animation_finished")
 	assert(get_tree().change_scene(path) == OK)
